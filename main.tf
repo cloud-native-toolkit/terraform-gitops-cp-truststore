685a30178f5af6bd275f55a9edfcea10ab91557e
Sean Sundberg <seansund@us.ibm.com>
2022-02-01 14:57:58 -0600

    Initial logic for IBM truststore deployment
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 locals {
-  name          = "my-module"
+  crd_name      = "truststore-crd"
+  name          = "truststore"
   bin_dir       = module.setup_clis.bin_dir
+  crd_yaml_dir  = "${path.cwd}/.tmp/${local.crd_name}/chart/${local.name}"
   yaml_dir      = "${path.cwd}/.tmp/${local.name}/chart/${local.name}"
-  service_url   = "http://${local.name}.${var.namespace}"
-  values_content = {
-  }
+  crd_layer = "infrastructure"
   layer = "services"
   type  = "base"
   application_branch = "main"
@@ -16,17 +16,56 @@ module setup_clis {
   source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
 }
 
-resource null_resource create_yaml {
+resource null_resource create_crd_yaml {
   provisioner "local-exec" {
-    command = "${path.module}/scripts/create-yaml.sh '${local.name}' '${local.yaml_dir}'"
+    command = "${path.module}/scripts/create-yaml.sh '${local.crd_name}' '${path.module}/charts/ibm-truststore-mgr-crds' '${local.crd_yaml_dir}'"
+  }
+}
+
+resource null_resource setup_crd_gitops {
+  depends_on = [null_resource.create_crd_yaml]
+
+  triggers = {
+    name = local.crd_name
+    namespace = var.namespace
+    yaml_dir = local.crd_yaml_dir
+    server_name = var.server_name
+    layer = local.crd_layer
+    type = local.type
+    git_credentials = yamlencode(var.git_credentials)
+    gitops_config   = yamlencode(var.gitops_config)
+    bin_dir = local.bin_dir
+  }
+
+  provisioner "local-exec" {
+    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
 
     environment = {
-      VALUES_CONTENT = yamlencode(local.values_content)
+      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
+      GITOPS_CONFIG   = self.triggers.gitops_config
+    }
+  }
+
+  provisioner "local-exec" {
+    when = destroy
+    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
+
+    environment = {
+      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
+      GITOPS_CONFIG   = self.triggers.gitops_config
     }
   }
 }
 
-resource null_resource setup_gitops {
+resource null_resource create_yaml {
+  depends_on = [null_resource.setup_crd_gitops]
+
+  provisioner "local-exec" {
+    command = "${path.module}/scripts/create-yaml.sh '${local.name}' '${path.module}/charts/ibm-truststore-mgr' '${local.yaml_dir}'"
+  }
+}
+
+resource null_resource setup_crd_gitops {
   depends_on = [null_resource.create_yaml]
 
   triggers = {
--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -4,8 +4,10 @@ SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
 MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
 
 NAME="$1"
-DEST_DIR="$2"
+CHART_DIR="$2"
+DEST_DIR="$3"
 
-## Add logic here to put the yaml resource content in DEST_DIR
+mkdir -p "${DEST_DIR}"
+cp -R "${CHART_DIR}" "${DEST_DIR}"
 
 find "${DEST_DIR}" -name "*"
